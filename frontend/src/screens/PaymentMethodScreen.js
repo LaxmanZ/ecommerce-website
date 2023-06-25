7b1c8e23a40603dbdf5a6a40cb6cf61b3337cc33
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-06-25 22:38:32 +0530

    fifth commit

---
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,69 @@
+import { Helmet } from 'react-helmet-async';
+import CheckoutSteps from '../components/CheckoutSteps';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/esm/Button';
+import { useContext, useEffect, useState } from 'react';
+import { Store } from '../store';
+import { useNavigate } from 'react-router-dom';
+
+export default function PaymentMethodScreen() {
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress, paymentMethod },
+  } = state;
+
+  const [paymentMethodName, setPaymentMethod] = useState(
+    paymentMethod || 'Paypal'
+  );
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate('/shipping');
+    }
+  }, [shippingAddress, navigate]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    ctxDispatch({
+      type: 'SAVE_PAYMENT_METHOD',
+      payload: paymentMethodName,
+    });
+    localStorage.setItem('paymentMethod', paymentMethodName);
+    navigate('/placeorder');
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <div className="container small-container">
+        <Helmet>
+          <title>Payment Method</title>
+        </Helmet>
+        <h1 className="my-3">Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              label="Paypal"
+              id="Paypal"
+              value="Paypal"
+              checked={paymentMethodName === 'Paypal'}
+              onChange={(e) => setPaymentMethod(e.target.va)}
+            />
+          </div>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              label="Stripe"
+              id="Stripe"
+              value="Stripe"
+              checked={paymentMethodName === 'Stripe'}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Button type="submit">Continue</Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -1,38 +1,40 @@
+import Axios from 'axios';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/esm/Button';
+import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import Axios from 'axios';
-import { useState, useContext, useEffect } from 'react';
-import { Store } from '../store';
+import { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { getError } from '../utils';
+import { Store } from '../store';
 
-
-export default function SigninScreen() {
+export default function SignupScreen() {
   const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get('redirect');
   const redirect = redirectInUrl ? redirectInUrl : '/';
 
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
-
   const submitHandler = async (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+      return;
+    }
     try {
-      const { data } = await Axios.post('/api/users/signin', {
+      const { data } = await Axios.post('/api/users/signup', {
+        name,
         email,
         password,
       });
-      ctxDispatch({
-        type: 'USER_SIGNIN',
-        payload: data,
-      });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
       localStorage.setItem('userInfo', JSON.stringify(data));
       navigate(redirect || '/');
     } catch (err) {
@@ -49,10 +51,15 @@ export default function SigninScreen() {
   return (
     <Container className="small-container">
       <Helmet>
-        <title>Sing in</title>
+        <title>Sign Up</title>
       </Helmet>
-      <h1 className="my-3">Sign In</h1>
+      <h1 className="my-3">Sign Up</h1>
       <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control onChange={(e) => setName(e.target.value)} required />
+        </Form.Group>
+
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control
@@ -68,15 +75,21 @@ export default function SigninScreen() {
             required
             onChange={(e) => setPassword(e.target.value)}
           />
+          <Form.Group className="mb-3" controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
         </Form.Group>
         <div className="mb-3">
-          <Button variant="primary" type="submit">
-            Sign In
-          </Button>
+          <Button type="submit">Sign Up</Button>
         </div>
         <div className="mb-3">
-          New Customer?{' '}
-          <Link to={`/signup?redirect=${redirect}`}>Create Your Acount</Link>
+          Already have an account?{' '}
+          <Link to={`/signin?redirect=${redirect}`}>Sign-In</Link>
         </div>
       </Form>
     </Container>
